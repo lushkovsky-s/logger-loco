1d2daf039d0606e7e0e82f725e69f145389a9350
Sergey Lushkovsky <s.lushkovsky@gmail.com>
2020-03-28 02:51:10 +0300

    v 0.0.5: Fixed interpolation, --> support, classes support

--- logger_loco/main.py
+++ logger_loco/main.py
@@ -1,9 +1,11 @@
 import inspect
 import re
 import uuid
+import functools
 
+current_indent = 0
 
-def loco(logger):
+def loco(logger, indent_symbol=' ', indent_size=2):
   rules = {
     '#@': 'debug',
     '#-': 'info',
@@ -13,39 +15,64 @@ def loco(logger):
 
   random = str(uuid.uuid4()).replace('-', '_')
 
-  def decorator(f):
-    lines = inspect.getsource(f)
-    new_lines = []
+  def decorator(something):
+    if inspect.isclass(something):
+      c = something
 
-    inject_n = 0
-    injects = {}
+      members = inspect.getmembers(c, predicate=inspect.isfunction)
 
-    for line in lines.split('\n'):
-      if line.startswith('@'):
-        continue
+      for name, method in members:
+        setattr(c, name, decorator(method))
 
-      for trigger, method in rules.items():
-        m = re.match(f'^(.+){trigger}(.+)$', line)
-        if m:
-          indent = m.group(1)
-          content = m.group(2)
+      return c
+    else:
+      f = something
 
-          injects[f'inject_{inject_n}'] = content
+      lines = inspect.getsource(f)
+      new_lines = []
 
-          line = "{}logger_{}.{}(inject_{})".format(indent, random, method, inject_n)
+      injects = {}
+      extra_indention_len = ''
 
-          inject_n += 1
+      global current_indent
 
-      new_lines.append(line)
+      for line in lines.split('\n'):
+        if line.startswith('@'):
+          continue
+        if 'def' in line and ':' in line:
+          m = re.match('( *)def.+:.*', line)
+          extra_indention_len = len(m.group(1))
+        if '-->' in line:
+          current_indent += 1
+          continue
+        if '<--' in line:
+          current_indent -= 1
+          continue
 
-    new_source = '\n'.join(new_lines)
+        line = line[extra_indention_len:]
 
-    generated = {
-      f'logger_{random}': logger,
-      **f.__globals__,
-      **injects
-    }
-    exec(new_source, generated)
+        for trigger, method in rules.items():
+          m = re.match(f'^(.+){trigger}(.+)$', line)
+          if m:
+            indent = m.group(1)
+            content = m.group(2)
 
-    return generated[f.__name__]
+            content = content.replace('\'', '\u2019')
+            content = indent_symbol * (current_indent * indent_size) + content
+
+            line = "{}logger_{}.{}(f'{}')".format(indent, random, method, content)
+
+        new_lines.append(line)
+
+      new_source = '\n'.join(new_lines)
+
+      generated = {
+        f'logger_{random}': logger,
+        **f.__globals__,
+        **injects
+      }
+      exec(new_source, generated)
+
+      return generated[f.__name__]
+
   return decorator
--- test.py
+++ test.py
@@ -28,9 +28,34 @@ def myfunc(a, b, x=MYCONST):
   #X Error
 
   #@ Inject {a} + {b} = {a + b}
-  #@ External var: {external_var)
+  #@ External var: {external_var}
   #@ Comment with manny backslashes \\\\\\ and quotes '''
+
+  #-->
+  #@ Indented
+  #<--
+  #@ Not indented
 
   return a + b
 
+@loco(logger, indent_size=4)
+def fun2():
+  #-->
+  #@ Indented with 4 spaces
+  #-->
+  #@ Indented deeper
+  #<--
+  #<--
+  #@ Regular
+  ...
+
 myfunc(1, 2)
+fun2()
+
+@loco(logger)
+class MyCalss(object):
+  def method_a(self, a, b):
+    #- Works inside class method a + b = {a + b}
+    ...
+
+MyCalss().method_a(1, 2)
